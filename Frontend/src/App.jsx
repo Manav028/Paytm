087c98856593edfa81cbb4a257beca36b865e3d0
= <=>
2024-07-22 22:39:51 +0100

    frontend still is ready

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1> Manav ready....................</h1>
+    <p> Manav is good developer and also shitty developer as per karishma </p>
+    </>
+  )
+}
+
+export default App
